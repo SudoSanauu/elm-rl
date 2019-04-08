70e7d1642c2193890c2c68b19f2d59534dc5139f
Aaron Jacobson <aaron.the.cow@gmail.com>
2019-04-07 22:31:38 -0700

    change Grid to record and add access functions

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,4 +1,10 @@
-module Grid exposing (makeGrid)
+module Grid exposing
+  ( makeGrid
+  , accessCell
+  , rows
+  , columns
+  , cells
+  )
 -- only expose what is needed
 
 import Cell as C
@@ -6,9 +12,44 @@ import Array as A
 
 -- This is hidden 
 -- x y 1D array Cell (as 2d array)
-type Grid =
-  G Int Int (A.Array C.Cell)
+type alias Grid =
+  { width : Int
+  , height : Int
+  , cells : (A.Array C.Cell)
+  }
 
 makeGrid : Int -> Int -> C.Cell -> Grid
-makeGrid width height copyCell =
-  G width height (A.repeat (width*height) copyCell)
+makeGrid inWidth inHeight copyCell =
+  { width = inWidth
+  , height = inHeight
+  , cells = (A.repeat (inWidth*inHeight) copyCell)
+  }
+
+accessCell : Int -> Int -> Grid -> Maybe C.Cell
+accessCell x y inGrid =
+  let
+    index = (x * inGrid.width) + y
+  in
+    A.get (index) inGrid.cells
+
+rows : Grid -> A.Array (A.Array C.Cell)
+rows inGrid =
+  let
+    sepRows rowNum array = 
+      A.slice (rowNum * inGrid.width) ((rowNum+1) * inGrid.width) array
+    useArray = A.initialize inGrid.height identity
+  in 
+    A.map (\x -> sepRows x inGrid.cells) useArray 
+
+columns : Grid -> A.Array (A.Array C.Cell)
+columns inGrid = 
+  let
+    sepCols colNum array =
+      A.slice (colNum * inGrid.height) ((colNum+1) * inGrid.width) array
+    useArray = A.initialize inGrid.width identity
+  in
+    A.map (\x -> sepCols x inGrid.cells) useArray
+
+cells : Grid -> A.Array C.Cell
+cells inGrid =
+  inGrid.cells
